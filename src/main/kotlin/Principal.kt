d1581dca4b19216f04566cc448ecf32a0603c72f
ArcangelSKY <angelzamoranooses@hotmail.com>
2021-03-27 11:42:27 -0300

    cambio de variables

--- src/main/kotlin/Principal.kt
+++ src/main/kotlin/Principal.kt
@@ -35,9 +35,11 @@ fun proyecto3() {
 }
 
 fun proyecto4() {
-    val peso1 = 89.4f
-    val peso2 = 67f
-    val peso3 = 87.45f
+    val peso1 = 12.4f
+    val peso2 = 30f
+    val peso3 = 100.45f
     val promedio = (peso1 + peso2 + peso3) / 3
     println("El promedio de los tres pesos de personas es $promedio")
+
+    // cambio de valores en la variable
 }
